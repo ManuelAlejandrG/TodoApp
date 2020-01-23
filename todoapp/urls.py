4c32c700cada9f80c2f9c69a61f24c923ac61f5c
ManuelAlejandrG <manuelgoyo10@gmail.com>
2020-01-23 14:37:34 -0400

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,30 @@
+"""todoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from todolist.views import index
+from users.views import base,login,welcome,register,logout
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',base,name="base" ),
+    path('todo',index, name="todolist"),
+    path('register', register, name="register"),
+    path('login', login, name="login"),
+    path('logout', logout,name="logout"),
+]
